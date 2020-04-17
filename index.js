b32411d8d7e3eecd15e353deef6ae02b8e76f1a2
Ajit Mehrotra <ajitmehrotra2009@gmail.com>
2020-04-17 18:24:26 -0400

    Add horizontal, vertical walls and goal

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const cells = 3; //rows/columns we're using a square maze for now so it doesn't matter
-const width = 1200;
-const height = 500;
-
+const cells = 4; //rows/columns we're using a square maze for now so it doesn't matter
+const width = 600;
+const height = 600;
+const unitLength = width / cells;
 const engine = Engine.create();
 const { world } = engine;
 
@@ -21,10 +21,10 @@ Runner.run(Runner.create(), engine);
 
 //Walls
 const walls = [
-  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 2, height, { isStatic: true }),
 ];
 
 World.add(world, walls);
@@ -115,3 +115,45 @@ const stepThroughCell = (row, col) => {
 };
 
 stepThroughCell(startRow, startCol);
+
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) {
+      return; // no need to draw a rectangle if there are no walls there
+    }
+
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength / 2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      10,
+      { isStatic: true }
+    );
+    World.add(world, wall);
+  });
+});
+
+verticals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) {
+      return;
+    }
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength,
+      rowIndex * unitLength + unitLength / 2,
+      10,
+      unitLength,
+      { isStatic: true }
+    );
+    World.add(world, wall);
+  });
+});
+
+const goal = Bodies.rectangle(
+  width - unitLength / 2,
+  height - unitLength / 2,
+  unitLength * 0.7,
+  unitLength * 0.7,
+  { isStatic: true }
+);
+World.add(world, goal);
